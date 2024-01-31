466bfc775d2deeb7c35d3579dd32ed6c36e8ceb1
ChauveSouris <desseobscurite@gmail.com>
2024-02-01 01:57:32 +0400

    Created StartButton

--- CoffeeCraft/StartViewController.swift
+++ CoffeeCraft/StartViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class StartViewController: UIViewController {
+//MARK: - StartViewController
+final class StartViewController: UIViewController {
+    
+    private var startButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupView()
+      
     }
 
 
 }
 
+//MARK: - Settings View
+private extension StartViewController {
+    func setupView() {
+        view.backgroundColor = .systemBrown
+        
+        addSubViews()
+        setupStartButton()
+        setupLayout()
+    }
+    
+}
+
+//MARK: - Settings
+private extension StartViewController {
+    func addSubViews() {
+        view.addSubview(startButton)
+    }
+    
+    func setupStartButton(){
+        startButton.setTitle("Create a drink", for: .normal)
+        startButton.setTitleColor(.black, for: .normal)
+        startButton.layer.cornerRadius = 15
+        startButton.backgroundColor = .white
+        startButton.titleLabel?.font = UIFont.systemFont(ofSize: 30, weight: .medium)
+    }
+    
+}
+
+//MARK: - Layout
+private extension StartViewController {
+    func setupLayout() {
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+}
