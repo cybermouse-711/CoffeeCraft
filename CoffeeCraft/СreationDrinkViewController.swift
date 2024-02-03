f36428878d09c9633c7067469ea7ab1e85da899c
ChauveSouris <desseobscurite@gmail.com>
2024-02-03 17:37:39 +0400

    Created CreationDrinkVC

---
+++ CoffeeCraft/СreationDrinkViewController.swift
@@ -0,0 +1,17 @@
+//
+//  СreationDrinkViewController.swift
+//  CoffeeCraft
+//
+//  Created by Елизавета Медведева on 03.02.2024.
+//
+
+import UIKit
+
+final class СreationDrinkViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
