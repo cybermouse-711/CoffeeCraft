ccc16f8cb998cf991ea893439f969edd0a7d1e89
Liza <desseobscurite@gmail.com>
2024-02-16 20:29:21 +0400

    Created extension for UILabel

---
+++ CoffeeCraft/Extensions/Extensions + UILabel.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions + UILabel.swift
+//  CoffeeCraft
+//
+//  Created by Elizaveta Medvedeva on 16.02.24.
+//
+
+import UIKit
+
+// MARK: - Extensions for UILabel
+extension UILabel {
+    func getElementLabel(for label: UILabel, with text: String) {
+        let label = label
+        
+        label.text = text
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textAlignment = .left
+        label.numberOfLines = 1
+    }
+}
