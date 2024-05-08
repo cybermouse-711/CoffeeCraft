f8ec8c030650b56b686d8aae83c04f0a766b2a24
Liza <desseobscurite@gmail.com>
2024-05-08 19:29:19 +0400

    Rename class StorageManager in NetworkManager

--- CoffeeCraft/Services/NetworkManager.swift
+++ CoffeeCraft/Services/NetworkManager.swift
@@ -1,5 +1,5 @@
 //
-//  StorageManager.swift
+//  NetworkManager.swift
 //  CoffeeCraft
 //
 //  Created by Elizaveta Medvedeva on 17.03.24.
@@ -8,20 +8,20 @@
 import Foundation
 
 //MARK: - Protocols
-protocol IStorageManager {
-    func fetchStorage<T: Decodable>(_ type: T.Type, _ file: String, _ extensionFile: String, completion: @escaping(Result<T, Error>) -> Void)
+protocol INetworkManager {
+    func fetchNetwork<T: Decodable>(_ type: T.Type, _ file: String, _ extensionFile: String, completion: @escaping(Result<T, Error>) -> Void)
 }
 
 ///Класс для парсинга JSON файлов
-//MARK: - StorageManager
-final class StorageManager {
+//MARK: - NetworkManager
+final class NetworkManager {
     
     private let jsonDecoder = JSONDecoder()
 }
 
 //MARK: - Extensions
-extension StorageManager: IStorageManager {
-    func fetchStorage<T>(_ type: T.Type, _ file: String, _ extensionFile: String, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable {
+extension NetworkManager: INetworkManager {
+    func fetchNetwork<T>(_ type: T.Type, _ file: String, _ extensionFile: String, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable {
         guard let url = Bundle.main.url(forResource: file, withExtension: extensionFile) else { return }
         
         do {
