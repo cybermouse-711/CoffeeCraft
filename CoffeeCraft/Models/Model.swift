f4e388dcb4067c88e0c8e075a5236654d60a0a9b
Liza <desseobscurite@gmail.com>
2024-07-08 19:22:30 +0400

    Fixed model

--- CoffeeCraft/Models/Model.swift
+++ CoffeeCraft/Models/Model.swift
@@ -5,89 +5,85 @@
 //  Created by Елизавета Медведева on 01.02.2024.
 //
 
-//MARK: - Dishes for JSON
-///Модель для парсинга JSON с типом посуды
-struct Dishes: Decodable {
-    let id: String
-    let dishes: String
-    let grinding: [String]
-    let shots: Int
-    let recipe: String
-}
-
-//MARK: - TypeCoffee for JSON
-///Модель для парсинга JSON с рецептом кофе
-struct TypeCoffee: Decodable {
-    let id: String
-    let milk: Bool
-    let type: String
-    let shots: Int
-    let recipe: String
-    let fact: String
-}
+////MARK: - Dishes for JSON
+/////Модель для парсинга JSON с типом посуды
+//struct Dishes: Decodable {
+//    let id: String
+//    let dishes: String
+//    let grinding: [String]
+//    let shots: Int
+//    let recipe: String
+//}
+//
+////MARK: - TypeCoffee for JSON
+/////Модель для парсинга JSON с рецептом кофе
+//struct TypeCoffee: Decodable {
+//    let id: String
+//    let milk: Bool
+//    let type: String
+//    let shots: Int
+//    let recipe: String
+//    let fact: String
+//}
 
 //MARK: - Created Drink for Scene
 ///Модель для формирования экрана с выбором опций
 struct Drink {
     ///Посуда для приготовления кофе
-    let dishes: Dishes
+    let dishes: [String]
     ///Тип кофе
-    let type: TypeCoffee
+    let type: [String]
+    ///Тип напитка
+    let coffee: [String]
     ///Сорт кофе
-    let variety: Grains
+    let grains: [String]
     ///Обжарка зерен
-    let roasting: Roasting
+    let roasting: [String]
     ///Тонкость помола
-    let grinding: Dishes
-    ///Молоко
-    let milk: TypeCoffee
-    ///Добавить сахар
-    let sugar = false
-    ///Добавить  мед
-    let honey = false
-    ///Добавить специи
-    let spices = false
-    ///Добавить сироп
-    let syrup = false
-    ///Температура напитка
-    let ice = false
+    let grinding: [String]
+    ///Ингредиенты
+    let ingredients: [String: Bool]
     
-    //MARK: Coffee grains
-    enum Grains: String, CaseIterable {
-        case arabica = "Arabica"
-        case robusta = "Robusta"
-        case liberica = "Liberica"
-        case excelsa = "Excelsa"
-        ///По умолчанию
-        case defalt = "Default"
-    }
-
-    //MARK: Roasting beans
-    enum Roasting: String, CaseIterable {
-        case light = "Light"
-        case medium = "Medium"
-        case mediumDark = "Medium-Dark"
-        case dark = "Dark"
-        ///По умолчанию
-        case defalt = "Default"
-    }
+//    ///Добавить сахар
+//    let sugar = false
+//    ///Добавить  мед
+//    let honey = false
+//    ///Добавить специи
+//    let spices = false
+//    ///Добавить сироп
+//    let syrup = false
+//    ///Температура напитка
+//    let ice = false
 }
+
+//MARK: - Recipe Drink for Scene
+///Модель для формирования экрана с рецептом
+//struct Recipe {
+//    ///Приготовление эспрессо на основе посуды
+//    let espresso: Dishes
+//    ///Количество полученных порций эспрессо
+//    let shotsEspresso: Dishes
+//    ///Приготовление определенного типа кофе на основе эспрессо
+//    let coffee: TypeCoffee
+//    ///Количество порций эспрессо для приготовления напитка
+//    let shotsCoffee: TypeCoffee
+//    ///Приготовление напитка на основе кофе
+//    let drink: String
+//    ///Изображение с посудой для кофе
+//    let image: String
+//    ///Факт о кофе
+//    let fact: TypeCoffee
+//}
 
 //MARK: - Recipe Drink for Scene
 ///Модель для формирования экрана с рецептом
 struct Recipe {
     ///Приготовление эспрессо на основе посуды
-    let espresso: Dishes
-    ///Количество полученных порций эспрессо
-    let shotsEspresso: Dishes
+    let espresso: String
     ///Приготовление определенного типа кофе на основе эспрессо
-    let coffee: TypeCoffee
-    ///Количество порций эспрессо для приготовления напитка
-    let shotsCoffee: TypeCoffee
+    let coffee: String
     ///Приготовление напитка на основе кофе
     let drink: String
     ///Изображение с посудой для кофе
     let image: String
-    ///Факт о кофе
-    let fact: TypeCoffee
 }
