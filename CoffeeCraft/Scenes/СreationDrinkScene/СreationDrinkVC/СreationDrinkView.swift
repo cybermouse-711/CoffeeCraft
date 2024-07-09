494b10e03b34d270f5f26ce0674bed7c7f888664
Liza <desseobscurite@gmail.com>
2024-07-09 18:05:06 +0400

    Deleted TableView and TableViewCelll

--- CoffeeCraft/Scenes/СreationDrinkScene/СreationDrinkVC/СreationDrinkView.swift
+++ CoffeeCraft/Scenes/СreationDrinkScene/СreationDrinkVC/СreationDrinkView.swift
@@ -13,7 +13,6 @@ final class СreationDrinkView: UIView {
     
     //MARK: Private Properties
     private let stackView = UIStackView()
- //   private let tableView = UITableView(frame: .zero, style: .grouped)
     private let createButton = CustomButton(Titles.createButton, .maxi)
     
     //Фейковые значения, подлежат удалению после парсинга джейсона
@@ -39,7 +38,6 @@ private extension СreationDrinkView {
         
         setupConctraints()
         
- //       setupTableView()
         setupSelfView()
         setupStackView()
         setupCreateButton()
@@ -72,18 +70,6 @@ private extension СreationDrinkView {
             CustomStackView("label2", true)
         )
     }
-//
-//    func setupTableView() {
-//        tableView.backgroundColor = UIColor(named: ColorSet.white)
-//        
-//        tableView.dataSource = self
-//        tableView.delegate = self
-//        
-//        tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
-//        tableView.rowHeight = UITableView.automaticDimension
-//        tableView.estimatedRowHeight = 44
-//        
-//    }
     
     func setupCreateButton() {
  
@@ -106,72 +92,10 @@ private extension СreationDrinkView {
             make.bottom.equalTo(createButton).offset(-150)
         }
         
-//        tableView.snp.makeConstraints { make in
-//            make.top.equalTo(safeAreaLayoutGuide)
-//            make.trailing.equalTo(safeAreaLayoutGuide)
-//            make.leading.equalTo(safeAreaLayoutGuide)
-//            make.bottom.equalTo(createButton).offset(-150)
-//        }
-        
         createButton.snp.makeConstraints { make in
             make.centerX.equalTo(self)
             make.bottom.equalTo(self).offset(-80)
         }
     }
 }
-
-////MARK: - UITableViewDataSource
-//extension СreationDrinkView: UITableViewDataSource {
-//    
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        2
-//       }
-//    
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        section == 0 ? TitlesofSection.titleforHeader0 : TitlesofSection.titleforHeader1
-//    }
-//    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        section == 0 ? dict2.keys.count : dict1.keys.count
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-//        
-//        guard let customCell = cell as? TableViewCell else { return UITableViewCell() }
-//        
-//        switch indexPath.section {
-//        case 0:
-//            dict2.sorted( by:  {$0.key < $1.key} )
-//            customCell.configureForMenu(indexPath, dict2)
-//        case 1:
-//            dict1.sorted( by:  {$0.key < $1.key} )
-//            customCell.configureForSwitch(indexPath, dict1)
-//        default:
-//            break
-//        }
-//        return customCell
-//    }
-//}
-//
-////MARK: - UITableViewDelegate
-//extension СreationDrinkView: UITableViewDelegate {
-//    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        tableView.deselectRow(at: indexPath, animated: true)
-//  
-//        guard let cell = tableView.cellForRow(at: indexPath) as? TableViewCell else { return }
-//
-//        switch indexPath.section {
-//        case 0:
-//            cell.tapOnMenu(indexPath, dict2)
-//            tableView.reloadRows(at: [indexPath], with: .automatic)
-//        case 1:
-//            cell.tapOnSwitch(indexPath, dict1)
-//            tableView.reloadRows(at: [indexPath], with: .automatic)
-//        default:
-//            break
-//        }
-//    }
-//}
 
