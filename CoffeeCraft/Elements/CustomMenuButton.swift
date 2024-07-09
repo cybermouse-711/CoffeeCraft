2dc3af3804cb9fd7dec0511f79a337f275f177e6
Liza <desseobscurite@gmail.com>
2024-07-09 17:02:34 +0400

    Created CustomMenuButton

---
+++ CoffeeCraft/Elements/CustomMenuButton.swift
@@ -0,0 +1,43 @@
+//
+//  CustomMenuButton.swift
+//  CoffeeCraft
+//
+//  Created by Elizaveta Medvedeva on 09.07.24.
+//
+
+import UIKit
+
+//MARK: - CustomMenuButton
+///Класс для настройки кастомного меню
+final class CustomMenuButton: UIButton {
+    
+    //MARK: Private properties
+//    private var menuButton = UIMenu()
+    private var children = [UIAction]()
+    private let actionClosure = { (action: UIAction) in
+        print(action.title)
+    }
+    
+    //MARK: Init
+    init(_ title: String, _ actions: [String]) {
+        super.init(frame: .zero)
+        
+        setTitle(title, for: .normal)
+        setTitleColor(UIColor(named: ColorSet.brown), for: .normal)
+        titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        
+        for action in actions {
+            children.append(UIAction(title: action, state: .on, handler: actionClosure))
+        }
+        
+        let menuButton = UIMenu(title: title, children: children)
+        
+        menu = menuButton
+        showsMenuAsPrimaryAction = true
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
