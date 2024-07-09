5c8674c1f722ce30976310d430636d404a075e6e
Liza <desseobscurite@gmail.com>
2024-07-09 17:02:03 +0400

    Changes setupTableView on setupStackView

--- CoffeeCraft/Scenes/СreationDrinkScene/СreationDrinkVC/СreationDrinkView.swift
+++ CoffeeCraft/Scenes/СreationDrinkScene/СreationDrinkVC/СreationDrinkView.swift
@@ -12,7 +12,8 @@ import SnapKit
 final class СreationDrinkView: UIView {
     
     //MARK: Private Properties
-    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let stackView = UIStackView()
+ //   private let tableView = UITableView(frame: .zero, style: .grouped)
     private let createButton = CustomButton(Titles.createButton, .maxi)
     
     //Фейковые значения, подлежат удалению после парсинга джейсона
@@ -38,8 +39,9 @@ private extension СreationDrinkView {
         
         setupConctraints()
         
-        setupTableView()
+ //       setupTableView()
         setupSelfView()
+        setupStackView()
         setupCreateButton()
     }
 }
@@ -51,22 +53,35 @@ private extension СreationDrinkView {
     }
     
     func addSubViews() {
-        [tableView, createButton].forEach {
+        [stackView, createButton].forEach {
             addSubview($0)
         }
     }
     
-    func setupTableView() {
-        tableView.backgroundColor = UIColor(named: ColorSet.white)
+    func setupStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 5
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 44
-        
+        stackView.addSubview(
+            CustomStackView("label1", ["action1", "action2"])
+        )
+        stackView.addSubview(
+            CustomStackView("label", true)
+        )
     }
+//
+//    func setupTableView() {
+//        tableView.backgroundColor = UIColor(named: ColorSet.white)
+//        
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//        
+//        tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
+//        tableView.rowHeight = UITableView.automaticDimension
+//        tableView.estimatedRowHeight = 44
+//        
+//    }
     
     func setupCreateButton() {
  
@@ -82,12 +97,19 @@ private extension СreationDrinkView {
 private extension СreationDrinkView {
     func setupConctraints() {
         
-        tableView.snp.makeConstraints { make in
+        stackView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide)
             make.trailing.equalTo(safeAreaLayoutGuide)
             make.leading.equalTo(safeAreaLayoutGuide)
             make.bottom.equalTo(createButton).offset(-150)
         }
+        
+//        tableView.snp.makeConstraints { make in
+//            make.top.equalTo(safeAreaLayoutGuide)
+//            make.trailing.equalTo(safeAreaLayoutGuide)
+//            make.leading.equalTo(safeAreaLayoutGuide)
+//            make.bottom.equalTo(createButton).offset(-150)
+//        }
         
         createButton.snp.makeConstraints { make in
             make.centerX.equalTo(self)
@@ -96,59 +118,58 @@ private extension СreationDrinkView {
     }
 }
 
-//MARK: - UITableViewDataSource
-extension СreationDrinkView: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        2
-       }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        section == 0 ? TitlesofSection.titleforHeader0 : TitlesofSection.titleforHeader1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        section == 0 ? dict2.keys.count : dict1.keys.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        guard let customCell = cell as? TableViewCell else { return UITableViewCell() }
-        
-        switch indexPath.section {
-        case 0:
-            dict2.sorted( by:  {$0.key < $1.key} )
-            customCell.configureForMenu(indexPath, dict2)
-        case 1:
-            dict1.sorted( by:  {$0.key < $1.key} )
-            customCell.configureForSwitch(indexPath, dict1)
-        default:
-            break
-        }
-        return customCell
-    }
-}
-
-//MARK: - UITableViewDelegate
-extension СreationDrinkView: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-  
-        guard let cell = tableView.cellForRow(at: indexPath) as? TableViewCell else { return }
-
-        switch indexPath.section {
-        case 0:
-            cell.tapOnMenu(indexPath, dict2)
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        case 1:
-            cell.tapOnSwitch(indexPath, dict1)
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        default:
-            break
-        }
-    }
-}
-
+////MARK: - UITableViewDataSource
+//extension СreationDrinkView: UITableViewDataSource {
+//    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        2
+//       }
+//    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        section == 0 ? TitlesofSection.titleforHeader0 : TitlesofSection.titleforHeader1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        section == 0 ? dict2.keys.count : dict1.keys.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//        
+//        guard let customCell = cell as? TableViewCell else { return UITableViewCell() }
+//        
+//        switch indexPath.section {
+//        case 0:
+//            dict2.sorted( by:  {$0.key < $1.key} )
+//            customCell.configureForMenu(indexPath, dict2)
+//        case 1:
+//            dict1.sorted( by:  {$0.key < $1.key} )
+//            customCell.configureForSwitch(indexPath, dict1)
+//        default:
+//            break
+//        }
+//        return customCell
+//    }
+//}
+//
+////MARK: - UITableViewDelegate
+//extension СreationDrinkView: UITableViewDelegate {
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//  
+//        guard let cell = tableView.cellForRow(at: indexPath) as? TableViewCell else { return }
+//
+//        switch indexPath.section {
+//        case 0:
+//            cell.tapOnMenu(indexPath, dict2)
+//            tableView.reloadRows(at: [indexPath], with: .automatic)
+//        case 1:
+//            cell.tapOnSwitch(indexPath, dict1)
+//            tableView.reloadRows(at: [indexPath], with: .automatic)
+//        default:
+//            break
+//        }
+//    }
+//}
 
